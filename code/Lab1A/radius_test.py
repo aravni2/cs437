7697b10123b127a421834267f52df91148326db7
Anthony R <aravni2@gmail.com>
2024-09-21 12:40:58 -0500

    fully calibrated radii

--- code/Lab1A/radius_test.py
+++ code/Lab1A/radius_test.py
@@ -1,10 +1,49 @@
 import picar_4wd as fc
 import time
 
+# test 360 for calibration
+# fc.turn_left(70)
+# time.sleep(5)
+# fc.stop()
+
+# fc.turn_right(70)
+# time.sleep(5)
+# fc.stop()
+
+# test 270
+# fc.turn_left(70)
+# time.sleep(5/4*3)
+# fc.stop()
+
+# time.sleep(1)
+
+# fc.turn_right(70)
+# time.sleep(5/4*3)
+# fc.stop()
+
+
+# test 180
+# fc.turn_left(70)
+# time.sleep(5/2)
+# fc.stop()
+
+# time.sleep(1)
+
+# fc.turn_right(70)
+# time.sleep(5/2)
+# fc.stop()
+
+
+# # test 90
 fc.turn_left(70)
-time.sleep(5)
+time.sleep(5/4)
 fc.stop()
 
-fc.turn_left(70)
-time.slee(5/4)
+
+time.sleep(1)
+
+fc.turn_right(70)
+time.sleep(5/4)
 fc.stop()
+
+
