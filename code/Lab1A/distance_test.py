44b070c205558855540fcff5d30dbbaaa6e89196
Tony <aravni2@gmail.com>
2024-09-21 14:26:10 -0500

    test timing

---
+++ code/Lab1A/distance_test.py
@@ -0,0 +1,14 @@
+import time
+import picar_4wd as fc
+
+# start_time = time.time()
+# # fc.forward(25)
+# time.sleep(4)
+
+
+# print (end_time-start_time)
+
+
+while (end_time-start_time)<=4:
+    fc.forward(25)
+    end_time= time.time()
